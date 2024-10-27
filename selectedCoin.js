dd0739a1a2e1f3b4df2ad36326f5b5c5f14a136a
naveendoddi <prasadnaveen847@gmail.com>
2024-10-27 18:29:58 +0530

    pushing

--- selectedCoin.js
+++ selectedCoin.js
@@ -221,6 +221,7 @@ function drawCandleSticks(data) {
   chart.draw(data, options);
 }
 
+
 function drawPriceChart1(priceData) {
   var data = google.visualization.arrayToDataTable(priceData);
   var options = {
